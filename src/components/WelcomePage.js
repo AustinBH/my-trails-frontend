2817fab5fd5381c9871458d7a1e53465c0b51dce
AustinBH <harlowaustin2@gmail.com>
2019-08-08 17:26:27 -0700

    added placeholder geotag search

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Button } from 'semantic-ui-react';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+
 
 const WelcomePage = props => {
     return (
         <>
             <h1>Hike Amie</h1>
             <img className='home-image' src='https://images.freeimages.com/images/large-previews/c27/mount-rainier-1337100.jpg' alt='mount-rainier' />
+            <Router>
+                <Button as={NavLink} color='brown' exact to='/hikes-near-me' content='Hikes Near Me!' />
+                <Route />
+            </Router>
         </>
     )
 }
