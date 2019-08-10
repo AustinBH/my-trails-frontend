b70c727243494148d00921826bdc6ba53629764d
AustinBH <harlowaustin2@gmail.com>
2019-08-09 23:34:06 -0700

    added full geolocation functionality, able to get hikes near user location

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,26 +1,43 @@
-import React from 'react'
-import { Button } from 'semantic-ui-react';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import React, { useState } from 'react'
+import { Button, Segment, Dimmer, Loader } from 'semantic-ui-react';
+import SearchResults from '../containers/SearchResults';
+import { api } from '../services/api';
 
 
 const WelcomePage = props => {
 
+    const [trails, setTrails] = useState([])
+    const [loading, setLoading] = useState(false)
+
     const getLocation = () => {
+        setLoading(true)
         navigator.geolocation ? navigator.geolocation.getCurrentPosition(logPostition) : alert('Geolocation not supported')
     }
 
     const logPostition = (position) => {
-        console.log(position.coords)
+        let lat = position.coords.latitude
+        let long = position.coords.longitude
+        api.trails.getTrailsByLocation(lat, long).then(json => setTrails(json)).then(setLoading(false))
     }
 
     return (
         <>
             <h1>Hike Amie</h1>
             <img className='home-image' src='https://images.freeimages.com/images/large-previews/c27/mount-rainier-1337100.jpg' alt='mount-rainier' />
-            <Router>
-                <Button onClick={getLocation} as={NavLink} color='brown' exact to='/hikes-near-me' content='Hikes Near Me!' />
-                <Route />
-            </Router>
+            <Button onClick={getLocation} color='brown' content='Hikes Near Me!' />
+            {loading ? 
+                <div className='info-holder'>
+                    <Segment className='info-loader'>
+                        <Dimmer active>
+                            <Loader>Getting Hikes...</Loader>
+                        </Dimmer>
+                    </Segment>
+                </div> 
+            :
+                <div className='user-search-holder'>
+                    <SearchResults trails={trails} /> 
+                </div>
+            }
         </>
     )
 }
