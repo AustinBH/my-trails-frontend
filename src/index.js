20d473c68cbdd1df14dc655d9ecc406c13129917
AustinBH <harlowaustin2@gmail.com>
2019-08-22 09:17:31 -0700

    added compose in or statement to allow for redux devtools on chrome but not break in safari

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { applyMiddleware, createStore, combineReducers } from 'redux';
+import { applyMiddleware, createStore, combineReducers, compose } from 'redux';
 import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
@@ -12,7 +12,11 @@ import manageCompletedHikes from './reducers/manageCompletedHikes';
 
 const rootReducer = combineReducers({user: manageUser, avatars: manageAvatars, favorites: manageFavorites, completedHikes: manageCompletedHikes})
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const enhancer = composeEnhancers(applyMiddleware(thunk))
+
+const store = createStore(rootReducer, enhancer)
 
 ReactDOM.render(
     <Provider store={store}>
